f5125a36ffc74708bb6abaed633aacc581059071
HardenSG <yandeqiang_i@didiglobal.com>
2024-02-16 17:41:48 +0800

    chore: init初始化仓库

---
+++ common examples【development】/scripts/release.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# release信息
+gitHelper() {
+    git config user.email "2767525216@qq.com"
+    git config user.name "HardenSG"
+    node ./scripts/gitHelper.js
+}
+
+# changelog生成器
+logGenertor() {
+    npm run changelog
+}
+
+# 更新版本信息
+updateVersion() {
+    node ./scripts/updateVersion.js
+}
+
+# 主逻辑
+main() {
+    echo "===== changelog & commit ====="
+
+    echo "1. 更新版本信息....."
+    updateVersion
+
+    echo "2. 生成changelog....."
+    logGenertor
+
+    echo "3. git actions"
+    gitHelper
+
+    echo "release success🏅!!"
+}
+
+main
---
+++ common examples【development】/scripts/updateVersion.js
@@ -0,0 +1,36 @@
+const inquirer = require('inquirer');  
+const { countVersion, fileUtils } = require('./utils')
+
+const pkg = fileUtils()
+async function release() {
+    const version = pkg.version
+    const versionUpdateTactics = await chooseUpdateTactics()
+    if (versionUpdateTactics) { 
+        pkg.version = countVersion(version, versionUpdateTactics)
+        fileUtils({
+            action: 'write',
+            content: JSON.stringify(pkg, null, 2)
+        })
+        fileUtils({
+            action: 'write',
+            rootFile: '../lib/copy/package.json',
+            content: JSON.stringify(pkg, null, 2)
+        })
+    }
+}
+
+// inquirer选择版本更新策略
+async function chooseUpdateTactics() {
+    const options = await inquirer.prompt([
+        {
+            type: 'list',
+            name: 'tactics',
+            message: '版本更新策略？默认patch',
+            choices: ['patch', 'beta', 'next'],
+            default: 'JavaScript'
+        },
+    ]);
+    return options.tactics
+}
+
+release();
