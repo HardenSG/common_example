f4d5d966c39a61286368922ac5a101e0dd2f13db
HardenSG <yandeqiang_i@didiglobal.com>
2024-02-16 17:39:34 +0800

    chore: init初始化仓库

---
+++ 常见用例【开发版】/scripts/gitHelper.js
@@ -0,0 +1,22 @@
+const exec = require('exec-sh')
+const { fileUtils, baseConfirmInq } = require('./utils')
+const { logUtils } = require('./constant')
+
+// git commit & tag
+const gitHelper = async () => {
+    const isNeedTag = await baseConfirmInq('是否需要tag？')
+    const pkg = fileUtils()
+
+    await exec.promise('git add .');
+    await exec.promise(`git commit -m "chore(release): ${pkg.version} feature release"`)
+    await exec.promise('git pull')
+    await exec.promise(`git push`)
+    if (isNeedTag) {
+        logUtils.warn('需要tag，推送远程标签中.....');
+        await exec.promise(`git tag v${pkg.version}`);
+        await exec.promise('git push origin --tags');
+        logUtils.success('推送远程成功🏅');
+    }
+}
+
+gitHelper()
