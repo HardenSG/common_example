4cfabd23a8d646e1d8a7ef203dcbdb1ff8219843
HardenSG <2767525216@qq.com>
2024-02-19 00:40:36 +0800

    chore(release): 1.2.0 feature release

---
+++ release-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	"monorepo": {
+		"needMonorepo": true,
+		"name": "packages"
+	},
+	"git": {
+		"branch": "main",
+		"email": "2767525216@qq.com",
+		"name": "HardenSG"
+	}
+};
